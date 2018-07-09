f624cd259c31bef5f161e7b1ec3063c3ce9c0f40
gmorpheme <github@gmorpheme.net>
2018-07-09 17:09:58 +0100

    Marginally improve error handling.
    Throw exception in IO when error during parse / desugar in embedded YAML.

--- src/Eucalypt/Syntax/Error.hs
+++ src/Eucalypt/Syntax/Error.hs
@@ -1,7 +1,7 @@
 module Eucalypt.Syntax.Error where
 
+import Control.Exception.Safe
 import Data.Void
-import Data.Typeable
 import qualified Text.Parsec.Error as P
 import qualified Text.Megaparsec as M
 
@@ -9,3 +9,5 @@ data SyntaxError
   = SyntaxError P.ParseError
   | MegaparsecError (M.ParseError (M.Token String) Void)
   deriving (Show, Eq, Typeable)
+
+instance Exception SyntaxError
