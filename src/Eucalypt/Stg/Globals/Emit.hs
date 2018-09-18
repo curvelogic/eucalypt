c12b7f3f2f370705a6f854639cb223cfa6d508a5
gmorpheme <github@gmorpheme.net>
2018-09-18 12:58:32 +0100

    Refactor emit locals to top level

--- src/Eucalypt/Stg/Globals/Emit.hs
+++ src/Eucalypt/Stg/Globals/Emit.hs
@@ -8,32 +8,61 @@ Stability   : experimental
 -}
 
 module Eucalypt.Stg.Globals.Emit
-  ( euRender
-  , euNull
+  ( globals
   ) where
 
 import Eucalypt.Stg.Syn
 import Eucalypt.Stg.Tags
 import Eucalypt.Stg.Intrinsics (intrinsicIndex)
 
+globals :: [(String, LambdaForm)]
+globals =
+  [ ("Emit.suppresses", suppresses)
+  , ("Emit.renderKV", renderKV)
+  , ("Emit.continueKVList", continueKVList)
+  , ("Emit.emptyList", emptyList)
+  , ("Emit.startList", startList)
+  , ("Emit.continueList", continueList)
+  , ("Emit.wrapBlock", wrapBlock)
+  , ("RENDER", euRender)
+  , ("NULL", euNull)
+  ]
 
+-- | __NULL - for emitting JSON / YAML null
 euNull :: LambdaForm
 euNull = standardConstructor 0 stgUnit
 
+emitMS :: StgSyn
+emitMS = appbif_ (intrinsicIndex "EMIT{") []
+
+emitME :: StgSyn
+emitME = appbif_ (intrinsicIndex "EMIT}") []
+
+emitSS :: StgSyn
+emitSS = appbif_ (intrinsicIndex "EMIT[") []
+
+emitSE :: StgSyn
+emitSE = appbif_ (intrinsicIndex "EMIT]") []
+
+emitScalar :: Ref -> StgSyn
+emitScalar n = appbif_ (intrinsicIndex "EMITx") [n]
+
+emitNull :: StgSyn
+emitNull = appbif_ (intrinsicIndex "EMIT0") []
 
 -- | LambdaForm used in 'euRender' to render a key/value pair.
 --
 -- Require env ref for 'suppresses' function
 renderKV :: LambdaForm
 renderKV =
-  lam_ 1 1 $
+  lam_ 0 1 $
   ann_ "renderKV" $
   casedef_
     (Atom arg)
     [ ( stgCons
       , ( 3
         , caselit_
-            (appfn_ suppressesRef [meta])
+            (appfn_ (Global "Emit.suppresses") [meta])
             [(NativeBool True, appcon_ stgUnit [])]
             (Just $
              case_
@@ -52,22 +81,27 @@ renderKV =
                           (Just (appcon_ stgUnit [])))))
                ])))
     ]
-    (appfn_ (Global "BOMB") [])
+    (appfn_
+       (Global "PANIC")
+       [Literal $ NativeString "Bad pair in Emit.renderKV"])
   where
-    suppressesRef = Local 0
-    arg = Local 1
-    key = Local 2
-    t = Local 3
-    meta = Local 4
-    val = Local 6
-    vval = Local 8
+    arg = Local 0
+    key = Local 1
+    t = Local 2
+    meta = Local 3
+    _suppressed = Local 4
+    val = Local 5
+    _valt = Local 6
+    vval = Local 7
 
 -- | LambdaForm that Inspects the metadata argument supplied to
 -- determine if it suppresses render.
+--
+-- __Emit.suppresses(m)
 suppresses :: LambdaForm
 suppresses =
   lam_ 0 1 $
-  ann_ "suppresses" $
+  ann_ "Emit.suppresses" $
   casedef_
     (Atom (Local 0))
     [ ( stgBlock
@@ -84,79 +118,66 @@ suppresses =
     ] $
   Atom $ Literal $ NativeBool False
 
+emptyList :: LambdaForm
+emptyList = value_ $ ann_ "emptyList" $ seq_ emitSS emitSE
+
+-- | Emit.continueList(l)
+continueList :: LambdaForm
+continueList =
+  lam_ 0 1 $
+  ann_ "Emit.continueList" $
+  casedef_
+    (Atom (Local 0))
+    [ ( stgCons
+      , ( 2
+        , seq_ (appfn_ (Global "RENDER") [Local 1]) $ appfn_ (Global "Emit.continueList") [Local 2]))
+    , (stgNil, (0, emitSE))
+    ]
+    (emitScalar (Local 1))
+
+-- | Emit.startList(l)
+startList :: LambdaForm
+startList =
+  lam_ 0 2 $
+  ann_ "Emit.startList" $
+  seq_
+    emitSS
+    (seq_
+       (appfn_ (Global "RENDER") [Local 0])
+       (appfn_ (Global "Emit.continueList") [Local 1]))
+
+-- | __Emit.continueKVList(l)
+continueKVList :: LambdaForm
+continueKVList =
+  lam_ 0 1 $
+  ann_ "Emit.continueKVList" $
+  case_
+    (Atom (Local 0))
+    [ ( stgCons
+      , ( 2
+        , seq_ (appfn_ (Global "Emit.renderKV") [Local 1]) $
+          appfn_ (Global "Emit.continueKVList") [Local 2]))
+    , (stgNil, (0, appcon_ stgUnit []))
+    ]
+
+-- | __Emit.wrapBlock(b)
+wrapBlock :: LambdaForm
+wrapBlock =
+  lam_ 0 1 $
+  ann_ "Emit.wrapBlock" $
+  seqall_ [emitMS, appfn_ (Global "Emit.continueKVList") [Local 0], emitME]
+
 
 -- | __RENDER(v)
 euRender :: LambdaForm
 euRender =
   lam_ 0 1 $
   ann_ "__RENDER" $
-  letrec_
-      -- emptyList
-    [ pc0_ $ value_ $ ann_ "emptyList" $ seq_ emitSS emitSE
-        -- continueList
-    , pc_ [continueList] $
-      lam_ 1 1 $
-      ann_ "continueList" $
-      casedef_
-        (Atom (Local 1))
-        [ ( stgCons
-          , ( 2
-            , seq_ (appfn_ (Global "RENDER") [Local 2]) $
-              appfn_ (Local 0) [Local 3]))
-        , (stgNil, (0, emitSE))
-        ]
-        (emitScalar (Local 2))
-        -- startList
-    , pc_ [continueList] $
-      lam_ 1 2 $
-      ann_ "startList" $
-      seq_
-        emitSS
-        (seq_
-           (appfn_ (Global "RENDER") [Local 1])
-           (appfn_ (Local 0) [Local 2]))
-        -- wrapBlock
-    , pc_ [continueKVList] $
-      lam_ 1 1 $
-      ann_ "wrapBlock" $ seqall_ [emitMS, appfn_ (Local 0) [Local 1], emitME]
-    , pc_ [suppressesRef] renderKV
-        -- continueKVList
-    , pc_ [renderKVRef, continueKVList] $
-      lam_ 2 1 $
-      ann_ "continueKVList" $
-      case_
-        (Atom (Local 2))
-        [ ( stgCons
-          , (2, seq_ (appfn_ (Local 0) [Local 3]) $ appfn_ (Local 1) [Local 4]))
-        , (stgNil, (0, appcon_ stgUnit []))
-        ]
-        -- typeSwitch
-    , pc_ [emptyList, continueList, startList, wrapBlock] $
-      lam_ 4 1 $
-      ann_ "typeSwitch" $
-      casedef_
-        (Atom (Local 4))
-        [ (stgBlock, (1, appfn_ (Local 3) [Local 5]))
-        , (stgCons, (2, appfn_ (Local 2) [Local 5, Local 6]))
-        , (stgNil, (0, appfn_ (Local 0) []))
-        , (stgUnit, (0, emitNull))
-        ]
-        (emitScalar (Local 5))
-    , pc0_ suppresses
+  casedef_
+    (Atom (Local 0))
+    [ (stgBlock, (1, appfn_ (Global "Emit.wrapBlock") [Local 1]))
+    , (stgCons, (2, appfn_ (Global "Emit.startList") [Local 1, Local 2]))
+    , (stgNil, (0, appfn_ (Global "Emit.emptyList") []))
+    , (stgUnit, (0, emitNull))
     ]
-    (appfn_ typeSwitch [Local 0])
-  where
-    emptyList = Local 1
-    continueList = Local 2
-    startList = Local 3
-    wrapBlock = Local 4
-    renderKVRef = Local 5
-    continueKVList = Local 6
-    typeSwitch = Local 7
-    suppressesRef = Local 8
-    emitMS = appbif_ (intrinsicIndex "EMIT{") []
-    emitME = appbif_ (intrinsicIndex "EMIT}") []
-    emitSS = appbif_ (intrinsicIndex "EMIT[") []
-    emitSE = appbif_ (intrinsicIndex "EMIT]") []
-    emitScalar n = appbif_ (intrinsicIndex "EMITx") [n]
-    emitNull = appbif_ (intrinsicIndex "EMIT0") []
+    (emitScalar (Local 1))
