85a33d0087833e1f8c9c7b0e02e662ca4f6e9f79
gmorpheme <github@gmorpheme.net>
2018-10-25 16:22:34 +0100

    Add text source

--- src/Eucalypt/Syntax/Input.hs
+++ src/Eucalypt/Syntax/Input.hs
@@ -72,6 +72,7 @@ inferFormat loc =
     extToFormat ext =
       case ext of
         ".json" -> Just "json"
+        ".txt" -> Just "text"
         ".toml" -> Just "toml"
         ".yaml" -> Just "yaml"
         ".yml" -> Just "yaml"
