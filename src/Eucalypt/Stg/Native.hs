669f9ba46c92a00492eb08c3350c3b26bf680510
gmorpheme <github@gmorpheme.net>
2019-02-03 23:10:55 +0000

    Eliminate hashability of Native

--- src/Eucalypt/Stg/Native.hs
+++ src/Eucalypt/Stg/Native.hs
@@ -13,7 +13,6 @@ module Eucalypt.Stg.Native where
 
 import Control.DeepSeq (NFData)
 import Data.Foldable (toList)
-import Data.Hashable
 import qualified Data.Map.Strict as MS
 import Data.Scientific
 import qualified Data.Set as S
@@ -34,21 +33,6 @@ data Native
   deriving (Eq, Show, Generic, Ord)
 
 instance NFData Native
-
--- | NativeBranchTable matches natives by hash map
-instance Hashable Native where
-  hashWithSalt s (NativeNumber n) = s `hashWithSalt` (0 :: Int) `hashWithSalt` n
-  hashWithSalt s (NativeString str) =
-    s `hashWithSalt` (1 :: Int) `hashWithSalt` str
-  hashWithSalt s (NativeSymbol sym) =
-    s `hashWithSalt` (2 :: Int) `hashWithSalt` sym
-  hashWithSalt s (NativeSet xs) =
-    S.foldl hashWithSalt (s `hashWithSalt` (4 :: Int)) xs
-  hashWithSalt s (NativeDict dict) =
-    MS.foldlWithKey
-      (\a k v -> a `hashWithSalt` k `hashWithSalt` v)
-      (s `hashWithSalt` (5 :: Int))
-      dict
 
 instance StgPretty Native where
   prettify (NativeNumber i) = either P.float P.int $ floatingOrInteger i
