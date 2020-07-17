f3d4c662d040ae00519b840d89228e593f234143
Greg Hawkins <github@gmorpheme.net>
2020-07-17 22:49:57 +0100

    Move from CircleCI to GitHub actions (#124)

--- ci/fetch.py
+++ ci/fetch.py
@@ -4,6 +4,7 @@ import sys
 from pathlib import Path
 import re
 import os
+import platform
 import subprocess
 import tarfile
 from github3 import login
@@ -12,7 +13,7 @@ def fetch(bindir):
 
     """ Download the latest binary release for this platform to `bindir`. """
 
-    token = os.environ['GITHUB_API_TOKEN']
+    token = os.environ.get('GITHUB_API_TOKEN') or os.environ.get('GITHUB_TOKEN')
     if not token:
         raise EnvironmentError("No Github API Token available")
 
@@ -20,7 +21,10 @@ def fetch(bindir):
     r = gh.repository("curvelogic", "eucalypt")
 
     release = r.latest_release()
-    asset = [a for a in release.assets() if "linux" in a.name][0]
+    if platform.system() == 'Darwin':
+        asset = [a for a in release.assets() if "osx" in a.name or "macOS" in a.name][0]
+    else:
+        asset = [a for a in release.assets() if "linux" in a.name][0]
 
     print("Downloading {}".format(asset.name))
     asset.download()
@@ -28,9 +32,9 @@ def fetch(bindir):
     print("Extracting {}".format(asset.name))
     with tarfile.open(asset.name, "r:gz") as tar:
         files = [f for f in tar if f.isreg()]
-        print(" - extracting {}".format(f.name for f in files))
+        print(" - extracting {}".format([f.name for f in files]))
         tar.extractall(members = files)
-        print(" - moving to ~/local/bin")
+        print(" - moving to {}".format(bindir))
         for f in files:
             os.rename(f.name,  bindir / Path(f.name).name)
     
