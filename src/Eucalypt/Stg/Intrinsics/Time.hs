71ce2b63e3331cbccbe0c7397f0a44d78d78cc62
gmorpheme <github@gmorpheme.net>
2020-07-21 22:26:27 +0100

    Intrinsics for ISO8601 parse / format.

--- src/Eucalypt/Stg/Intrinsics/Time.hs
+++ src/Eucalypt/Stg/Intrinsics/Time.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE RecordWildCards, OverloadedStrings, QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards, OverloadedStrings, QuasiQuotes, LambdaCase #-}
 {-|
 Module      : Eucalypt.Stg.Intrinsics.Time
 Description : Time and date intrinsics
-Copyright   : (c) Greg Hawkins, 2018
+Copyright   : (c) Greg Hawkins, 2018-2020
 License     :
 Maintainer  : greg@curvelogic.co.uk
 Stability   : experimental
@@ -13,10 +13,13 @@ module Eucalypt.Stg.Intrinsics.Time
   , toZonedDateTime
   ) where
 
+import Control.Applicative
 import Data.Dynamic
 import Data.Scientific
 import Data.Time.Calendar
+import Data.Time.Clock
 import Data.Time.Clock.POSIX
+import Data.Time.Format.ISO8601
 import Data.Time.LocalTime
 import Eucalypt.Stg.Error
 import Eucalypt.Stg.IntrinsicInfo
@@ -30,6 +33,8 @@ intrinsics =
   [ IntrinsicInfo "IFIELDS" 1 (invoke instantToUTCFields)
   , IntrinsicInfo "ZDT" 7 (invoke toZonedDateTime)
   , IntrinsicInfo "ZDT.FIELDS" 1 (invoke zdtFields)
+  , IntrinsicInfo "ZDT.PARSE8601" 1 (invoke parseIso8601)
+  , IntrinsicInfo "ZDT.FORMAT8601" 1 (invoke formatIso8601)
   ]
 
 
@@ -99,3 +104,63 @@ zdtFields :: MachineState -> Dynamic -> IO MachineState
 zdtFields ms zdtDyn = do
   zdt <- cast ms zdtDyn
   returnList ms $ zonedDateTimeFields zdt
+
+
+parseZoned :: FormatExtension -> Format ZonedTime
+parseZoned fe =
+  let fcal = calendarFormat fe
+      ftod = timeOfDayFormat fe
+   in zonedTimeFormat fcal ftod fe
+
+parseUTC :: FormatExtension -> Format UTCTime
+parseUTC fe =
+  let fcal = calendarFormat fe
+      ftod = timeOfDayFormat fe
+   in utcTimeFormat fcal ftod
+
+parseUTCNoSeconds :: FormatExtension -> Format UTCTime
+parseUTCNoSeconds fe =
+  let fcal = calendarFormat fe
+      ftod = hourMinuteFormat fe
+   in utcTimeFormat fcal ftod
+
+parseLocal :: FormatExtension -> Format LocalTime
+parseLocal fe =
+  let fcal = calendarFormat fe
+      ftod = timeOfDayFormat fe
+   in localTimeFormat fcal ftod
+
+parseLocalNoSeconds :: FormatExtension -> Format LocalTime
+parseLocalNoSeconds fe =
+  let fcal = calendarFormat fe
+      ftod = hourMinuteFormat fe
+   in localTimeFormat fcal ftod
+
+dayToZonedTime :: Day -> ZonedTime
+dayToZonedTime day =
+  ZonedTime
+    { zonedTimeToLocalTime =
+        LocalTime {localDay = day, localTimeOfDay = midnight}
+    , zonedTimeZone = utc
+    }
+
+-- | Parse an ISO8601 formatted date to a native datetime
+parseIso8601 :: MachineState -> String -> IO MachineState
+parseIso8601 ms dateString =
+  case parseFormatExtension parseZoned dateString <|>
+       (utcToZonedTime utc <$> parseFormatExtension parseUTC dateString) <|>
+       (utcToZonedTime utc <$> parseFormatExtension parseUTCNoSeconds dateString) <|>
+       (utcToZonedTime utc . localTimeToUTC utc <$>
+        parseFormatExtension parseLocal dateString) <|>
+       (utcToZonedTime utc . localTimeToUTC utc <$>
+        parseFormatExtension parseLocalNoSeconds dateString) <|>
+       (dayToZonedTime <$> parseFormatExtension calendarFormat dateString) of
+    Just zdt -> returnDynamic ms (zdt :: ZonedTime)
+    _ -> throwIn ms $ InvalidArgument "Unrecognised datetime format."
+
+
+-- | Format a native datetime as ISO8601
+formatIso8601 :: MachineState -> Dynamic -> IO MachineState
+formatIso8601 ms zdtDyn = do
+  zdt <- cast ms zdtDyn
+  return $ setCode ms $ (`ReturnLit` Nothing) $ NativeString (iso8601Show (zdt :: ZonedTime))
