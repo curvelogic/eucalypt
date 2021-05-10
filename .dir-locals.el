6217817f99cea1c5935a27f94ba056997352131c
gmorpheme <github@gmorpheme.net>
2021-05-10 11:39:36 +0100

    Remove Haskell implementation

--- .dir-locals.el
+++
@@ -1,9 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((haskell-mode
-  (intero-targets "eucalypt:lib" "eucalypt:exe:eu" "eucalypt:test:eucalypt-test")))
-
-
-
-((nil . ((projectile-project-compilation-cmd . "stack build --test --fast --file-watch --copy-bins --exec 'hlint .'"))))
